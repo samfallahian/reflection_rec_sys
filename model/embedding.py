2b34a3ea9011a252d0ae91e2e4357d89804a93ff
samfallahi <mfallahi@uncc.edu>
2022-09-12 15:49:54 -0400

    fix: prevent embedding past sentences

--- model/embedding.py
+++ model/embedding.py
@@ -5,10 +5,12 @@ class Embedding:
     def __init__(self, sentences_past):
         self.sentences_past = sentences_past
         self.model = SentenceTransformer('stsb-roberta-large')
+        self.embeded_past = self.model.encode(self.sentences_past, convert_to_tensor=True)
 
     def do_embedding(self, input_text):
         return self.model.encode(input_text, convert_to_tensor=True)
 
     def get_cosine_scores(self, challenge):
-        return util.pytorch_cos_sim(self.do_embedding(self.sentences_past), self.do_embedding(challenge)).numpy()
+        embedded_challenge = self.do_embedding(challenge)
+        return util.pytorch_cos_sim(self.embeded_past , embedded_challenge).numpy()
 
