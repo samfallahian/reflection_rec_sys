c8b6412b4355d467fa7bd2e200d254e8f917031f
Aileen Benedict <jelly8d@hotmail.com>
2022-09-21 12:58:51 -0400

    add: config for student name

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,6 @@
 CFG = {
     "data": {
-        "path": "/mnt/d/sources/reflection_rec_sys/data"
+        "path": "C:/Users/jelly/Projects/reflection_rec_sys/data"
     },
     "train": {
         "learning_rate": 1e-3,
@@ -8,7 +8,8 @@ CFG = {
         "buffer_size": 1000,
         "epochs": 200,
         "challenge_prompt": "challenge",
-        "solution_prompt": "solution"
+        "solution_prompt": "solution",
+        "feature_name": "name"
     },
     "model": {
         "input": [128, 128, 3]
