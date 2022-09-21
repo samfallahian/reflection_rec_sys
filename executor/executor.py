f053d3e369199ccf4e739b069d69d22d43cedf3c
samfallahi <mfallahi@uncc.edu>
2022-09-21 13:49:23 -0400

    feat: export results to csv file

--- executor/executor.py
+++ executor/executor.py
@@ -6,7 +6,7 @@ class Executor:
     def __init__(self, cfg, data):
         self.cfg = cfg
         self.data = data
-        self.sentences_past = data[cfg.challenge_prompt].values
+        self.sentences_past = data[cfg.train.challenge_prompt].values
         self.embedding = Embedding(sentences_past=self.sentences_past)
 
     def similarity(self, challenge):
@@ -17,10 +17,10 @@ class Executor:
         cosine_scores = self.similarity(challenge)
         new_df = pd.DataFrame()
         for i in range(len(self.sentences_past)):
-            new_df = new_df.append({'cos_score': cosine_scores[i][0],
-                                    'challenge': self.sentences_past[i],
-                                    'solution': self.data.iloc[i][self.cfg.solution_prompt],
-                                    'name' : self.data.iloc[i][self.cfg.feature_name]}
+            new_df = new_df.append({f"cos_score": cosine_scores[i][0],
+                                    f"challenge": self.sentences_past[i],
+                                    f"solution": self.data.iloc[i][self.cfg.train.solution_prompt],
+                                    f"name" : self.data.iloc[i][self.cfg.train.feature_name]}
                                    , ignore_index=True)
 
         # Filter out any responses that this student has written themselves
@@ -29,6 +29,13 @@ class Executor:
 
         return new_df.nlargest(k, ['cos_score'])
 
-    def get_results(self, challenge, name):
-        res = self.get_top_k(challenge, name)
-        return res
+    def get_results(self, input_file_name):
+        inputs = pd.read_csv(self.cfg.data.path+f"/inputs/{input_file_name}.csv", encoding="cp1252", engine='python')
+        results=[]
+        for index, row in inputs.iterrows():
+            top_k = self.get_top_k(challenge=row["challenge"], name=row["name"])
+            results.append(top_k)
+
+        df = pd.concat(results, ignore_index=True)
+        df.to_csv(self.cfg.data.path+f"/results/result_for_{input_file_name}.csv", index=False)
+        pass
--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from datetime import datetime
 
 
 def run():
+
     conf = Config.from_json(cfg.CFG)
     data_reader = DataReader(conf)
     print(f"Start reading csv files: {datetime.now()}")
     data = data_reader.load_and_standardize_data()
     print(f"Load the model: {datetime.now()}")
-    executor = Executor(cfg=conf.train, data=data)
+    executor = Executor(cfg=conf, data=data)
     print(f"Start training: {datetime.now()}")
 
-    print(executor.get_top_k(challenge="time management", name="Bob"))
+    executor.get_results(input_file_name="inputs1")
     print(f"Finished: {datetime.now()}")
 
 
 if __name__ == '__main__':
     run()
+
 
