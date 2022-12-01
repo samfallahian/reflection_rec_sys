05a7271f00c3f07bb56107460193c98d258a73f8
Aileen <abenedi3>
2022-12-01 17:31:21 -0500

    fix: column rename

--- dataloader/datareader.py
+++ dataloader/datareader.py
@@ -15,7 +15,7 @@ class DataReader:
             frame = pd.read_csv(csv, encoding="cp1252", engine='python')  # "ISO-8859-1" , 'utf-8'
             data.append(frame)
         df = pd.concat(data, ignore_index=True)
-        df.columns = ["name","semester","week","challenge","solution"]
+        df.columns = ["name","semester","reflection","challenge","solution"]
 
         # df = pd.read_csv(f"{path}/data1.csv", encoding="cp1252", engine='python')
         df.dropna(inplace=True)
