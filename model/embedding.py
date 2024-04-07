007702dcac9dc71b71ff967809022a9af8a12060
Aileen <abenedi3>
2024-04-07 14:19:25 -0400

    encoding and loading embeddings to save time

--- model/embedding.py
+++ model/embedding.py
@@ -1,11 +1,34 @@
 from sentence_transformers import SentenceTransformer, util
-
+import os
+import torch
 
 class Embedding:
-    def __init__(self, sentences_past):
+    def __init__(self, sentences_past, cfg):
         self.sentences_past = sentences_past
         self.model = SentenceTransformer('stsb-roberta-large')
-        self.embeded_past = self.model.encode(self.sentences_past, convert_to_tensor=True)
+        print("finished loading model")
+
+        self.cfg = cfg
+
+        # Specify and make folder for storing embeddings if doesn't already exist
+        self.embeddings_folder = f"{self.cfg.data.path}/data/embeddings"
+        os.makedirs(self.embeddings_folder, exist_ok=True)
+        print(f"Embeddings folder is: {self.embeddings_folder}")
+        # Specify file name for saved embeddings
+        filename = f"{self.cfg.data.course_input}.pt"
+        print(f"File name is now: {filename}")
+        # Full path for the embeddings file
+        self.embeddings_file = os.path.join(self.embeddings_folder, filename)
+
+        # Load embeddings if they exist, otherwise encode and save
+        if os.path.exists(self.embebeddings_file):
+            print("Loading saved embeddings")
+            self.embeded_past = torch.load(self.embeddings_file)
+        else:
+            print("Encoding sentences and saving embeddings")
+            self.embeded_past = self.model.encode(self.sentences_past, convert_to_tensor=True)
+        
+        print("Finished setup")
 
     def do_embedding(self, input_text):
         return self.model.encode(str(input_text), convert_to_tensor=True)
