81980af0ecff55121aecc16216ec34819650b983
Aileen <abenedi3>
2024-04-25 22:24:50 -0400

    convert config to yaml

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from dataloader.datareader import DataReader
-from configs import config as cfg
+#from configs import config as cfg
 from utils.helpers import Config
 from executor.executor import Executor
 from datetime import datetime
-
+import yaml
 
 def run():
 
-    conf = Config.from_json(cfg.CFG)
+    with open('configs/config.yaml', 'r') as file:
+        conf = yaml.safe_load(file)
+    #conf = Config.from_json(cfg.CFG)
     data_reader = DataReader(conf)
     print(f"Start reading csv files: {datetime.now()}")
     data = data_reader.load_and_standardize_data()
     print(f"Load the model: {datetime.now()}")
     executor = Executor(cfg=conf, data=data)
     print(f"Start training: {datetime.now()}")
-    executor.get_results(input_file_name=conf.data.course_input)
+    executor.get_results(input_file_name=conf['data']['course_input'])
     print(f"Finished: {datetime.now()}")
 
 
