aead84527f421706563c49dbfe054610f467c159
Aileen <abenedi3>
2024-04-25 22:25:32 -0400

    add param for whether we use all data or only one class's data

--- dataloader/datareader.py
+++ dataloader/datareader.py
@@ -8,15 +8,18 @@ class DataReader:
         self.cfg = cfg
 
     def load_and_standardize_data(self):
-        path = self.cfg.data.path
-        csv_files = glob.glob(os.path.join(f"{path}/data", "*.csv"))
-        data = []
-        for csv in csv_files:
-            frame = pd.read_csv(csv, encoding="cp1252", engine='python')  # "ISO-8859-1" , 'utf-8'
-            data.append(frame)
-        df = pd.concat(data, ignore_index=True)
-        df.columns = ["name","semester","reflection","challenge","solution"]
+        path = self.cfg['data']['path']
 
-        # df = pd.read_csv(f"{path}/data1.csv", encoding="cp1252", engine='python')
+        if self.cfg['data']['filter_class'] == False:
+            csv_files = glob.glob(os.path.join(f"{path}/data", "*.csv"))
+            data = []
+            for csv in csv_files:
+                frame = pd.read_csv(csv, encoding="cp1252", engine='python')  # "ISO-8859-1" , 'utf-8'
+                data.append(frame)
+            df = pd.concat(data, ignore_index=True)
+            df.columns = ["name","semester","reflection","challenge","solution"]
+        else:
+            df = pd.read_csv(f"{path}/data/{self.cfg['data']['course_input']} data.csv") # TODO Check this for improvements later
+
         df.dropna(inplace=True)
         return df
